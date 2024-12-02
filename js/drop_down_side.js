ef57c802bff77e43c945b8434a3ca744ba8dedb5
Fabien Holard <fabien.holard@epita.fr>
2024-12-03 00:39:05 +0100

    fix: behavior when a modal is opened

--- js/drop_down_side.js
+++ js/drop_down_side.js
@@ -1,9 +1,12 @@
 const toggleSideMenuButton = document.getElementById('toggle-side-menu');
 const sideMenu = document.getElementById('side-menu');
 const globalOverlay = document.getElementById('global-overlay');
+const modalOverlay = document.getElementById('modal-overlay');
 const openModalButtons = document.querySelectorAll('.open-modal');
 const modals = document.querySelectorAll('.modal');
 const closeModalButtons = document.querySelectorAll('.close-modal');
+
+let modal_opened = false;
 
 // Afficher ou masquer le menu et l'overlay global
 toggleSideMenuButton.addEventListener('click', () => {
@@ -16,7 +19,14 @@ openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modalId = button.getAttribute('data-modal');
         const modal = document.getElementById(modalId);
-        modal.style.display = 'block';
+        if (modal_opened) {
+            modals.style.display = 'none';
+        }
+        else {
+            modal.style.display = 'block';
+            modalOverlay.style.display = 'block';
+            modal_opened = true;
+        }
     });
 });
 
@@ -25,7 +35,18 @@ closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal');
         modal.style.display = 'none';
+        modalOverlay.style.display = 'none';
+        modal_opened = false;
     });
+});
+
+// Fermer la modale lorsque l'on clique en dehors de celle-ci
+modalOverlay.addEventListener('click', () => {
+    modals.forEach(modal => {
+        modal.style.display = 'none';
+    });
+    modalOverlay.style.display = 'none';
+    modal_opened = false;
 });
 
 // Fermer la modale lorsque l'on clique en dehors de celle-ci
